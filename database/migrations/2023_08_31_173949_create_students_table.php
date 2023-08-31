3e85d08495f1470cbfdd990fc832d3523e2ee65d
seducerooo <>
2023-08-31 22:05:51 +0330

    StudentController created && in student model fillable array added && student migration filled and migrated

--- database/migrations/2023_08_31_173949_create_students_table.php
+++ database/migrations/2023_08_31_173949_create_students_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('username');
+            $table->string('phone');
+            $table->string('dob');
             $table->timestamps();
         });
     }
